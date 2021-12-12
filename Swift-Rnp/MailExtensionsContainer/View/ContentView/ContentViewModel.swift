f9d476386f3575dd66dbfeb4621b1c5619b0e3b8
Sergey Vinogradov <wins.konar@gmail.com>
2021-12-13 01:34:33 +0200

    fix creation of keys on container app and use in plugin

--- Swift-Rnp/MailExtensionsContainer/View/ContentView/ContentViewModel.swift
+++ Swift-Rnp/MailExtensionsContainer/View/ContentView/ContentViewModel.swift
@@ -6,16 +6,32 @@
 //
 
 import Foundation
+import Combine
 
-class ContentViewModel {
+class ContentViewModel: ObservableObject {
+    @Published var hasKeys: Bool = false
     
     var listModel: KeysListViewModel {
         KeysListViewModel(manager: self.manager)
     }
     
     private var manager: KeysManager
+    private var cancellables = Set<AnyCancellable>()
     
     init(manager: KeysManager) {
         self.manager = manager
+        setupObservables()
+    }
+    
+    private func setupObservables() {
+        manager
+            .$items
+            .map{ !$0.isEmpty }
+            .assign(to: \.hasKeys, on: self)
+            .store(in: &cancellables)
+    }
+    
+    func addKeys() {
+        manager.addKeys()
     }
 }
