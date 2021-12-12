3df33ecc4a57f07adb6355491976facf480bff31
Sergey Vinogradov <wins.konar@gmail.com>
2021-12-12 18:46:26 +0200

    add mock for container app

--- Swift-Rnp/MailExtensionsContainer/ContentView.swift
+++ Swift-Rnp/MailExtensionsContainer/ContentView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+struct KeyFile: Identifiable {
+    let id = UUID()
+    let filename: String
+    let isPublic: Bool
+}
+
+extension KeyFile {
+    static var mock: [KeyFile] {
+        [
+            KeyFile(filename: "pubring.gpg", isPublic: true),
+            KeyFile(filename: "secring.gpg", isPublic: false)
+        ]
+    }
+}
+
 struct ContentView: View {
+    private let keyFiles = KeyFile.mock
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Keys manager")
+            
+            HStack {
+                Group {
+                    Button {} label: {
+                        Image(systemName: "plus.circle")
+                    }
+                    
+                    Button {} label: {
+                        Image(systemName: "minus.circle")
+                    }
+                    .disabled(true)
+                    
+                    Button {} label: {
+                        Image(systemName: "square.and.arrow.down")
+                    }
+                    
+                    Button {} label: {
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                    .disabled(true)
+                }
+                .font(.system(size: 40))
+                .buttonStyle(.borderless)
+            }
             .padding()
+            
+            Table {
+                TableColumn("Filename", value: \.filename)
+            } rows: {
+                ForEach(keyFiles) { value in
+                    TableRow(value)
+                }
+            }
+        }
     }
 }
 
