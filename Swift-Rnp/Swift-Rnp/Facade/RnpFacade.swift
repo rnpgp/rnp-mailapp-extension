081bb09a487c55621fc69a6fa0dd4675f010bf35
Sergey Vinogradov <wins.konar@gmail.com>
2021-12-14 22:30:52 +0200

    remove unused code

--- Swift-Rnp/Swift-Rnp/Facade/RnpFacade.swift
+++ Swift-Rnp/Swift-Rnp/Facade/RnpFacade.swift
@@ -32,37 +32,3 @@ class RnpFacade {
         wCreateKeys(RnpFacade.defaultKeysFormat, RnpFacade.defaultKeysFormat) == 1
     }
 }
-
-/*
-// Not working/stuck because incompatibility types of callback/enclosure on interloop - the origin of requirements of the wrapper.
- /// TODO: remove it after closest commit
-class Example {
-    /// this example function generates RSA/RSA and Eddsa/X25519 keypairs
-    static func generateKeys() {
-        let ffi = UnsafeMutablePointer<rnp_ffi_t?>.allocate(capacity: 1)
-        defer {
-            ffi.deallocate()
-        }
-
-
-//        var result = 1
-        // initialize FFI object
-        guard rnp_ffi_create(ffi, RnpKeyStoreFormat.gpg.rawValue, RnpKeyStoreFormat.gpg.rawValue) == RnpResult.success.rawValue else {
-            return
-        }
-        print(ffi)
-
-//        var ffi2: rnp_ffi_t = ffi
-//
-//        // set password provider
-////        guard let theFfi = ffi as? rnp_ffi_t? else { return }
-//        rnp_ffi_set_pass_provider(ffi2, { aFfi, app_ctx_up, rnp_key_handle_t_opt, cchar_up_opt, cchar_up_opt2, anInt in
-//            print()
-//            return false
-//        }, nil)
-//        if (rnp_ffi_set_pass_provider(ffi, example_pass_provider, NULL)) {
-//            goto finish;
-//        }
-    }
-}
- */
