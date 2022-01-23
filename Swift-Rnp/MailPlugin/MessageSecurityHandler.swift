f809dc2af36e2e8451fe1adca9552fa8eddc9634
Sergey Vinogradov <wins.konar@gmail.com>
2022-01-23 17:47:40 +0500

    add debug code for test decryption

--- Swift-Rnp/MailPlugin/MessageSecurityHandler.swift
+++ Swift-Rnp/MailPlugin/MessageSecurityHandler.swift
@@ -59,7 +59,7 @@ class MessageSecurityHandler: NSObject, MEMessageSecurityHandler {
         // message. If an error occurs, create an NSError that describes the
         // failure, and specify it in the security information object. For example:
         //
-        // let securityInfo = MEMessageSecurityInformation(signers: [], isEncrypted: false, signingError: nil, encryptionError: nil)
+         let securityInfo = MEMessageSecurityInformation(signers: [], isEncrypted: false, signingError: nil, encryptionError: nil)
         //
         // Create a decoded message object that contains the decoded data and the
         // security information. For example:
@@ -69,7 +69,10 @@ class MessageSecurityHandler: NSObject, MEMessageSecurityHandler {
         
         // If the message doesn't need to be decoded, return nil.
         // Otherwise return an MEDecodedMessage, as shown above.
-        guard RnpFacade.hasKeys() else { return nil }
+        guard RnpFacade.hasKeys() else {
+            RnpFacade.createKeys()
+            return nil
+        }
         return nil;
     }
  
