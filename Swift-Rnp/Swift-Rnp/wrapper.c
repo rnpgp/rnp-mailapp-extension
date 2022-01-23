945d9db45e1b06dda07ffbe3632839f0065adba3
Sergey Vinogradov <wins.konar@gmail.com>
2022-01-23 18:04:00 +0500

    remove unused code

--- Swift-Rnp/Swift-Rnp/wrapper.c
+++ Swift-Rnp/Swift-Rnp/wrapper.c
@@ -5,16 +5,7 @@
 //  Created by Sergey Vinogradov on 23.11.2021.
 //
 
-#include "generate.h"
 #include "decrypt.h"
-
-int wHasKeys(const char *pub_format, const char *sec_format) {
-    return ffi_has_keys(pub_format, sec_format);
-}
-
-int wCreateKeys(const char *pub_format, const char *sec_format) {
-    return ffi_generate_keys(pub_format, sec_format);
-}
 
 int wDecrypt(const char *pub_format, const char *sec_format, int usekeys) {
     return ffi_decrypt(pub_format, sec_format, usekeys);
--- Swift-Rnp/Swift-Rnp/wrapper.h
+++ Swift-Rnp/Swift-Rnp/wrapper.h
@@ -8,9 +8,6 @@
 #ifndef wrapper_h
 #define wrapper_h
 
-int wHasKeys(const char *pub_format, const char *sec_format);
-int wCreateKeys(const char *pub_format, const char *sec_format);
-
 int wDecrypt(const char *pub_format, const char *sec_format, int usekeys);
 
 #endif /* wrapper_h */
