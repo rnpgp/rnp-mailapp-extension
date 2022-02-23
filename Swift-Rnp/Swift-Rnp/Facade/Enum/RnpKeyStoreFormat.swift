7e29bdc6d63e5091b31153288831ed52dc5c94ef
Sergey Vinogradov <wins.konar@gmail.com>
2022-02-23 19:14:54 +0500

    remove unused files, make app work with xcframework

--- Swift-Rnp/Swift-Rnp/Facade/Enum/RnpKeyStoreFormat.swift
+++ Swift-Rnp/Swift-Rnp/Facade/Enum/RnpKeyStoreFormat.swift
@@ -5,19 +5,19 @@
 //  Created by Sergey Vinogradov on 02.12.2021.
 //
 
-enum RnpKeyStoreFormat: String {
-    case gpg, kbx, g10, g21
-    
-    var rawValue: String {
-        switch self {
-        case .gpg:
-            return RNP_KEYSTORE_GPG
-        case .kbx:
-            return RNP_KEYSTORE_KBX
-        case .g10:
-            return RNP_KEYSTORE_G10
-        case .g21:
-            return RNP_KEYSTORE_GPG21
-        }
-    }
-}
+//enum RnpKeyStoreFormat: String {
+//    case gpg, kbx, g10, g21
+//    
+//    var rawValue: String {
+//        switch self {
+//        case .gpg:
+//            return RNP_KEYSTORE_GPG
+//        case .kbx:
+//            return RNP_KEYSTORE_KBX
+//        case .g10:
+//            return RNP_KEYSTORE_G10
+//        case .g21:
+//            return RNP_KEYSTORE_GPG21
+//        }
+//    }
+//}
--- Swift-Rnp/Swift-Rnp/Swift-Rnp-Bridging-Header.h
+++ Swift-Rnp/Swift-Rnp/Swift-Rnp-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include "rnp_shared.h"
-#import "RnpObject.h"
+// TODO: Need separation for ARM and Intel here - not useful: #ifdef ARCHITECTURE_INTEL
+#import <RNPFramework_MacOS_Intel/RNPFramework_MacOS_Intel.h>
