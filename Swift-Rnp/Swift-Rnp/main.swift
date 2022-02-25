60b42ab3ac3c11d36809554c917f8c0bc89de824
Sergey Vinogradov <wins.konar@gmail.com>
2022-02-26 04:01:11 +0500

    remove unused files, make smoke test to work with latest spm

--- Swift-Rnp/Swift-Rnp/main.swift
+++ Swift-Rnp/Swift-Rnp/main.swift
@@ -9,16 +9,17 @@ import Foundation
 
 // TODO: next flags check isn't work as well #if ARCHITECTURE_INTEL, because it happen in bridhging header
 // FIXME: For the moment only Intel lib is ready
-
-var object = RnpObject.init(pubFormat: "GPG"/*RnpKeyStoreFormat.gpg.rawValue*/, secFormat: "GPG"/*RnpKeyStoreFormat.gpg.rawValue*/)
-// FIXME: Think about keychain
-let password = "userPass"
-if !object.hasKeys {
-    object.createKeys("userId@key", password: password) { success in
-        print("Keys are \(success ? "generated" : "not generated")")
+let format = RnpConstants.rnpKeyStoreFormat_(toString: .gpg)
+if let object = RnpObject.init(pubFormat: format, secFormat: format) {
+        // FIXME: Think about keychain
+    let password = "userPass"
+    if !object.hasKeys {
+        object.createKeys("userId@key", password: password) { success in
+            print("Keys are \(success ? "generated" : "not generated")")
+        }
     }
-}
-
-if object.hasKeys, let message = object.decrypt(usingKeys: true, password: password) {
-    print(message)
+    
+    if object.hasKeys, let message = object.decrypt(usingKeys: true, password: password) {
+        print(message)
+    }
 }
