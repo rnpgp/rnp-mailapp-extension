df987c57e4ad059933c5d2c64f1b5047c477f6a5
Sergey Vinogradov <wins.konar@gmail.com>
2022-05-06 23:20:09 +0300

    add verify detached sign feature, add decoder skeleton, convert facade to non-react design and fix to follow framework's latest changes

--- Swift-Rnp/Shared/RnpFacade.swift
+++ Swift-Rnp/Shared/RnpFacade.swift
@@ -6,58 +6,97 @@
 //
 
 import Foundation
-import Combine
-
-enum RnpFacadeError: Error {
-    case importKeyFail
-}
 
 class RnpFacade: ObservableObject {
     
-    @Published var hasKeys: Bool = false
+    var hasKeys: Bool
+    let password: String
     
-    private var cancellables = Set<AnyCancellable>()
-    private var keysFilesList: [String]
+    private var foreighKeysList: [String]
     
-    let object: RnpObject
+    private let object: RnpObject
     
     init(pubFormat: RnpKeyStoreFormat, secFormat: RnpKeyStoreFormat) {
         object = RnpObject(pubFormat: RnpConstants.rnpKeyStoreFormat_(toString: pubFormat),
                            secFormat: RnpConstants.rnpKeyStoreFormat_(toString: secFormat))
         
-        keysFilesList = UserDefaults.keysList
+        hasKeys = object.hasOwnKeys
         
-        // TODO: Implement physically removing files
-        // Remove all not-succesfully loaded files
-        keysFilesList = object.loadKeyFiles(keysFilesList).compactMap{ $0.value.boolValue ? $0.key : nil }
-        setupObservables()
+        if UserDefaults.password.isEmpty {
+            // Create password for future use for all keys inside
+            UserDefaults.password = String.randomString(length: 10)
+        }
+        password = UserDefaults.password
+        
+        foreighKeysList = UserDefaults.keysList
+        
+        // TODO: Implement managment for keys: valide by using | last using | filename. Also need to implement file deletion for unsuccessfully loaded ones.
+        foreighKeysList = object.loadKeyFiles(foreighKeysList).compactMap{ $0.value.boolValue ? $0.key : nil }
     }
     
-    private func setupObservables() {
-        object
-            .publisher(for: \.hasOwnKeys) // KVO
-            .assign(to: \.hasKeys, on: self)
-            .store(in: &cancellables)
-    }
-    
-    func createKeys(userId: String, password: String) {
-        guard !object.hasOwnKeys else { return }
+    func createKeys(userId: String, password: String) -> Result<Void, Error> {
+        guard !object.hasOwnKeys else {
+            hasKeys = true
+            return .success()
+        }
         
-        object.createKeys(userId, password: password, completion: {_ in });
+        do {
+            try object.createKeys(userId, password: password)
+            hasKeys = true
+            return .success()
+        } catch {
+            return .failure(error)
+        }
     }
     
     func checkKeyPresenceFor(_ email: String) -> Bool {
         object.isKeyExist(forUserId: email)
     }
     
+    func signMessageData(_ messageData: Data, userId: String) -> Result<Data, Error> {
+        do {
+            let string = try object.sign(messageData, userId: userId, password: password)
+            guard let data = string.data(using: .utf8) else {
+                return .failure(RnpFacadeError.signedMessageIsEmpty)
+            }
+            return .success(data)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    // TODO: return any signers info (into result) to deliver/check on upper level
+    func verifyMessageString(_ signedMessage: String, detachedSign: String) -> Result<Void, Error> {
+        do {
+            try object.verifyRawString(signedMessage, detachedSign: detachedSign)
+            return .success()
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func encryptMessageData(_ messageData: Data, userId: String, password: String?) -> Result<Data, Error> {
+        do {
+            let string = try object.encryptData(messageData, userId: userId, password: password)
+            guard let data = string.data(using: .utf8) else {
+                return .failure(RnpFacadeError.encryptedMessageIsEmpty)
+            }
+            return .success(data)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
     func decryptMessage(message: String) -> String? {
-        guard let decrypted = object.decryptString(message, usingKeys: true, password: nil) else { return nil }
+        guard let decrypted = try? object.decryptString(message, password: password) else { return nil }
         return decrypted
     }
     
     @discardableResult
-    func importKeyString(_ string: String) -> String? {
-        guard let filename = object.importKey(from: string) else { return nil }
+    func importKeyString(_ string: String, filename: String?) -> String? {
+        // TODO: deliver import error to UI
+        // TODO: Use filename if it present, in most cases it prevents copies for key files
+        guard let filename = try? object.importKey(from: string) else { return nil }
             
         addFilename(filename)
         return filename
@@ -66,13 +105,19 @@ class RnpFacade: ObservableObject {
         // MARK: - Private
     
     private func addFilename(_ name: String) {
-        keysFilesList.append(name)
-        UserDefaults.keysList = keysFilesList
+        foreighKeysList.append(name)
+        UserDefaults.keysList = foreighKeysList
     }
 }
 
 extension RnpFacade {
     static var mock: RnpFacade {
         RnpFacade(pubFormat: .gpg, secFormat: .gpg)
+    }
+}
+
+extension RnpFacade {
+    static func errorCodeCase(error: NSError) -> RnpErrorCode {
+        RnpConstants.rnpErrorCode(Int32((error as NSError).code))
     }
 }
